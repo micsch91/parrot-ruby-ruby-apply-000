db5f386f00fb76a1d416657d34d9b0ac564d239c
Michael Schneider <mic.sch91@gmail.com>
2020-03-16 18:28:32 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(s=Squawk, *p)
-puts s
-return s
-puts *p
-return *p
+  puts s
+  return s
+  puts *p
+  return *p
 end
